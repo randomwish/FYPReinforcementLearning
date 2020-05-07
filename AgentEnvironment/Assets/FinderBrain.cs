6976174621599421837920cebbbd89f3c3c06f79
randomwish <hanzhe100100@gmail.com>
2020-05-07 16:33:58 +0800

    Skeleton Implementation of functions

--- AgentEnvironment/Assets/FinderBrain.cs
+++ AgentEnvironment/Assets/FinderBrain.cs
@@ -3,38 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+
 public class FinderBrain : Agent
 {
-    public void Initialise()
-    {
+    public GameObject TargetArea;
+    TargetFinderArea m_TargetArea;
 
+    Rigidbody m_AgentRb;
+    
+    public override void Initialize()
+    {
+        m_AgentRb = GetComponent<Rigidbody>();
+        m_TargetArea = TargetArea.GetComponent<TargetFinderArea>();
+        //Add Environment Settings and Reset Params
     }
 
-    public override void CollectObservations(VectorSensor sensor)
-    {
-
-    }
-
-    public void MoveAgent(float[] act)
-    {
-
-    }
-    public override void Heuristic(float[] actionsOut)
+    public override void OnEpisodeBegin()
     {
 
     }
 
     public override void OnActionReceived(float[] vectorAction)
     {
+        MoveAgent(vectorAction);
+    }
+    public void MoveAgent(float[] act)
+    {
 
     }
-
-    void OnCollisionEnter(Collision other)
+    public override void CollectObservations(VectorSensor sensor)
     {
 
     }
 
+    public override void Heuristic(float[] actionsOut)
+    {
 
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {
+
+    }
 
 }
-
