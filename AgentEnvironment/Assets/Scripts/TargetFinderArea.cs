016a571aa62da5b49b8ea292dfddc889d585e902
chlwys <joshuaweeys@gmail.com>
2020-05-13 17:26:09 +0800

    3 agents (hardcoded)
    hardcoded to make it easier for now, can be changed to proper spawning later

--- AgentEnvironment/Assets/Scripts/TargetFinderArea.cs
+++ AgentEnvironment/Assets/Scripts/TargetFinderArea.cs
@@ -6,13 +6,18 @@ public class TargetFinderArea : MonoBehaviour
 {
     public GameObject target;
     public int numTargets;
+    public int numAgents;
     public float range;
 
     public GameObject agent;
+    public GameObject agent2;
+    public GameObject agent3;
+    // can change for mutliple agents later; testing first
     private List<GameObject> agentsList;
     private List<GameObject> targetsList;
 
     public List<GameObject> TargetsList { get { return targetsList;  } }
+    public List<GameObject> AgentsList { get { return agentsList;  } }
 
 
     public void RemoveSpecificTarget(GameObject targetObject)
@@ -63,19 +68,34 @@ public class TargetFinderArea : MonoBehaviour
  
     }
 
-    public void RespawnAgent()
-    {
-        Rigidbody rigidbody = agent.GetComponent<Rigidbody>();
-        rigidbody.velocity = Vector3.zero;
-        rigidbody.angularVelocity = Vector3.zero;
-        agent.transform.position = GenerateNewPosition(transform.position,range);
-        agent.transform.rotation = Quaternion.Euler(0f,UnityEngine.Random.Range(0f,180f),0f); 
+    public void RespawnAgent(int num, GameObject agents)
+    {/*
+        for (int i = 0; i < num; i++)
+        {
+            GameObject newAgent = Instantiate<GameObject>(agents.gameObject);
+
+            agentsList.Add(newAgent);
+        } */
+
+        List <GameObject> spawnList = new List<GameObject>();
+        spawnList.Add(agent);
+        spawnList.Add(agent2);
+        spawnList.Add(agent3);
+
+
+        foreach (GameObject element in spawnList) {
+            Rigidbody rigidbody = element.GetComponent<Rigidbody>();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+            element.transform.position = GenerateNewPosition(transform.position, range);
+            element.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 180f), 0f);
+        }
     }
 
     public void ResetArea()
     {
         RemoveAllTargets();
-        RespawnAgent();
+        RespawnAgent(numAgents, agent);
         SpawnTarget(numTargets,target);
     }
     private void Start()
