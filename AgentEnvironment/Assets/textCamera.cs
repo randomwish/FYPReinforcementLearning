3246552005d95269b7479c0ad96baba304e04f5a
Randomwish <hanzhe100100@gmail.com>
2020-08-04 23:09:04 +0800

    Created new indices for the target
    Utilised TextMeshPro to create ids for the targets for debugging purposes

---
+++ AgentEnvironment/Assets/textCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class textCamera : MonoBehaviour
+{
+
+    public GameObject objectToLookAt;
+    int targetID;
+    public TextMeshPro textMeshPro;
+    void Start()
+    {
+        targetID = objectToLookAt.GetComponent<ObjectLogic>().targetID;
+    }
+
+    void Update()
+    {
+        textMeshPro.SetText("{0}", targetID);
+        transform.LookAt(objectToLookAt.transform);
+        Quaternion q = transform.rotation;
+ 
+    }
+
+}
